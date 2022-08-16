ed88b8b3a795c22dbaa347d00948fbdc3bfbafe0
Himanshu Maurya <mhimanshu.338@gmail.com>
2022-08-16 15:27:49 +0530

    added partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+  integrations: [
+    tailwind(),
+    svelte(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
